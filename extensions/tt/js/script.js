4ccc7dc7620d4b1ad63a7fa6495b8b07ccd6d831
Mikael Wase <braskamin13@hotmail.com>
2012-10-13 00:25:38 +0200

    Fixed broken popup auto height and removed jquery from index.html

--- extensions/tt/js/script.js
+++ extensions/tt/js/script.js
@@ -1,19 +1,21 @@
 window.addEventListener('DOMContentLoaded', function() {
 	var bg = opera.extension.bgProcess;
-	bg.resizePopup(document.body.offsetHeight);
+	var tt = bg.tt;
+	tt.resizePopup(document.body.offsetHeight);
 });
 
 var step = 0;
 
 jQuery(function($) {
 	var bg = opera.extension.bgProcess;
+	var tt = bg.tt;
 	
 	$('.item').click(function() {
 		$(this).find('.item-details').animate({height: 'toggle'}, {
 			duration: 200, 
 			step: function(now,fx) {
 				if(step > 0) {
-					bg.resizePopup(document.body.offsetHeight);
+					tt.resizePopup(document.body.offsetHeight);
 				}
 				step++;
 			}, 
